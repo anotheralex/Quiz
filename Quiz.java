b3bf21684ecf9c7ee682d6bfdda92c1fe1a25822
Alex Wollenschlaeger <alexw@panix.com>
2014-04-03 10:45:28 +0100

    added method signatures for getters and setters

---
+++ Quiz.java
@@ -0,0 +1,41 @@
+package quiz;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * A quiz is a collection of questions, each with multiple answers, only one
+ * of which is correct
+ *
+ * A quiz has an ID, a name, a list of questions, with answers for each
+ * question, and a flag indicating if each answer is correct
+ *
+ */
+
+public interface Quiz extends Remote {
+
+	/**
+	 * Return the quiz ID
+	 *
+	 * @return id the id of the quiz
+	 */
+	int getId();
+
+	/**
+	 * Return the name of the quiz
+	 *
+	 * @return name the name of the quiz
+	 */
+	String getName();
+
+	/**
+	 * Add a new question (and associated potential answers) to a quiz
+	 */
+	void addQuestion();
+
+	/**
+	 * Print the questions in the current quiz
+	 *
+	 */
+	void printQuestions();
+}
