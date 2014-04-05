c952a8841c1ab07b81dac18dac950eea31494ab0
Alex Wollenschlaeger <alexw@panix.com>
2014-04-05 10:04:37 +0100

    updated comments

--- PlayerImpl.java
+++ PlayerImpl.java
@@ -8,6 +8,12 @@ public class PlayerImpl implements Player {
 	private int id;
 	private String name;
 
+	/**
+	 * Constructor for player
+	 *
+	 * @param id the id the player
+	 * @param name the name of the player
+	 */
 	public PlayerImpl(int id, String name) {
 		this.id = id;
 		this.name = name;
@@ -15,6 +21,8 @@ public class PlayerImpl implements Player {
 
 	/**
 	 * Getter for Player ID
+	 *
+	 * @return the id of the player
 	 */
 	public int getId() {
 		return this.id;
@@ -22,6 +30,8 @@ public class PlayerImpl implements Player {
 
 	/**
 	 * Getter for Player name
+	 *
+	 * @return the name of the Player
 	 */
 	public String getName() {
 		return this.name;
