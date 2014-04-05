e429d07bc4fceccb160866db06ef70e5a716054e
Alex Wollenschlaeger <alexw@panix.com>
2014-04-05 09:44:49 +0100

    implemented tests for getters

---
+++ PlayerImplTest.java
@@ -0,0 +1,33 @@
+package quiz;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PlayerImplTest {
+
+	private Player p;
+	private String name;
+	private int id;
+
+	@Before
+	public void setup() {
+		id = 1;
+		name = "Test";
+
+		p = new PlayerImpl(id, name);
+	}
+
+	@Test
+	public void getIdTest() {
+		int expected = id;
+		int actual = p.getId();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getNameTest() {
+		String expected = name;
+		String actual = p.getName();
+		assertEquals(expected, actual);
+	}
+}
