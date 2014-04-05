d23dc56f21b493ebe84a9b5a9e4e297acfd08064
Alex Wollenschlaeger <alexw@panix.com>
2014-04-05 09:44:21 +0100

    removed setter for name

---
+++ PlayerImpl.java
@@ -0,0 +1,17 @@
+package quiz;
+
+/**
+ * A player in the quiz
+ */
+public interface Player {
+
+	/**
+	 * Getter for Player ID
+	 */
+	void getId();
+
+	/**
+	 * Getter for Player name
+	 */
+	String getName();
+}
