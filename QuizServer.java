8cd4a113f42638a2b0ea4417d929afdee80a860d
Alex Wollenschlaeger <alexw@panix.com>
2014-04-07 09:40:24 +0100

    implemented listQuizzes()

--- QuizServer.java
+++ QuizServer.java
@@ -2,6 +2,7 @@
 
 import java.rmi.RemoteException;
 import java.rmi.UnicastRemoteObject;
+import java.lang.StringBuilder;
 
 /**
  * A quiz server that serves the QuizService service to remote clients
@@ -63,5 +64,18 @@ public class QuizServer extends UnicastRemoteObject implements QuizService {
 	 * List all available quizzes
 	 */
 	public void listQuizzes() {
+		if (this.quizzes.isEmpty()) {
+			System.out.println("No quizzes yet.");
+		} else {
+			System.out.println("ID\tName");
+			for (Quiz q : quizzes) {
+				StringBuilder sb = new StringBuilder();
+				sb.append("ID: ");
+				sb.append(q.getId());
+				sb.append("\t");
+				sb.append(q.getName());
+				System.out.println(sb.toString());
+			}
+		}
 	}
 }
